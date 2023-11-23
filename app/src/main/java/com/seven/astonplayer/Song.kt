ed5d81b44ebedbb1757a6e9fd3c91c1b9773e9e6
Zabalkanskiy <azabalkanskij@hotmail.com>
2023-11-23 15:21:24 +0300

    change Song class add field resID

--- app/src/main/java/com/seven/astonplayer/Song.kt
+++ app/src/main/java/com/seven/astonplayer/Song.kt
@@ -1,3 +1,4 @@
 package com.seven.astonplayer
 
-class Song(val iD: Long, val title: String, val artist: String)
+class Song(val iD: Long, val title: String, val artist: String = "", val resID: Int)
+
