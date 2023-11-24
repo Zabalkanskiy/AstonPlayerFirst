58b320110ef0222b2d7396b4f07fb702ffecca6c
Zabalkanskiy <azabalkanskij@hotmail.com>
2023-11-24 11:14:41 +0300

    change compile version and sdk version on 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.seven.astonplayer"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.seven.astonplayer"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
